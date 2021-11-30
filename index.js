71335968906dcae3645093d85029e80617c833d4
satyaprakashsinghrathour <satyapsr13@gmail.com>
2021-11-30 13:26:13 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const app = require('express')()
+const http = require('http').createServer(app)
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+    res.send("Node Server is running. Yay!!")
+})
+
+//Socket Logic
+const socketio = require('socket.io')(http)
+
+socketio.on("connection", (userSocket) => {
+    userSocket.on("send_message", (data) => {
+        userSocket.broadcast.emit("receive_message", data)
+    })
+})
+
+http.listen(port);
